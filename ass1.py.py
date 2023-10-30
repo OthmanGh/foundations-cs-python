00250574fcb93e5658d0a6a871db5e5600037d4a
OTHMAN AL GHANDDOUR <othmanghandour51@gmail.com>
2023-10-30 20:42:28 +0300

    adding assignment 1

---
+++ ass1.py.py
@@ -0,0 +1,52 @@
+# #* Exercise 1 : 
+# top = float(input("Top Value : "))
+# bottom = float(input("Bottom Value : "))
+# value = float(input("User Value: "))
+
+# if value >= bottom and value <= top: 
+#     print("True")
+# else:
+#     print("False")
+
+
+# #* Exercise 2 : 
+# a = int(input("Enter a : "))
+# b = int(input("Enter b : "))
+# c = int(input("Enter c : "))
+
+# if a > b and a > c:
+#     print(f"The maximum value is {a}")
+#     if b > c:
+#         print(f"The minimum value is {c}")
+#     else:
+#         print(f"The minimum value is {b}")
+# elif b > a and b > c:
+#     print(f"The maximum value is {b}")
+#     if a > c:
+#         print(f"The minimum value is {c}")
+#     else:
+#         print(f"The minimum value is {a}")
+# elif c > a and c > b:
+#     print(f"The maximum value is {c}")
+#     if a > b:
+#         print(f"The minimum value is {b}")
+# else:
+#         print(f"The minimum value is {a}")
+
+
+# # #* Exercise 3 : 
+# user_grade = int(input("Enter your total grade : "))
+
+# if user_grade > 100 or user_grade < 0:
+#     print("Please enter a valid grade")
+# else:
+#     if user_grade <= 59:
+#         print('F')
+#     elif user_grade <= 69:
+#         print("D")
+#     elif user_grade <=79:
+#         print("C")
+#     elif user_grade <= 89:
+#         print("B")
+#     else:
+#         print("A")
