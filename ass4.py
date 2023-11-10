a66fab20bd7666fd7b83530593c27113a41476fc
Othman Al Ghandour <othmanghandour51@gmail.com>
2023-11-10 23:25:16 +0300

    Begin work on Assignment 4

---
+++ ass4.py
@@ -0,0 +1,21 @@
+# You are writing a system for a transport company.
+# This company has a list of cities, and a list of drivers. Each driver has their specific route.
+# For example, if the route of the driver “Alex” is Beirut, Zahle, it means that he will go to Beirut and then Zahle.
+# Write a system that repeatedly prompts the user with the following options:
+# Enter:
+# 1.	To add a city
+# 2.	To add a driver
+# 3.	To add a city to the route of a driver
+# 4.	Remove a city from a driver’s route
+# 5.	To check the deliverability of a package
+# If the user inputs 1, they are asked the name of the city. This name (if valid) is added to the list of cities.
+# If the user input 2, they are asked the name of the driver and then the name of the cities they will visit (how you will ask for the route if up to you)
+# If the user inputs 3, they are prompted the name of the driver and then the name of the city to add. If all is well, the user is then given the following options:
+# Input:
+# 0.	To add to the beginning of the route
+# -1. To add to the end of the route
+# #. (any other number) to add that city to the given index
+# If the user inputs 4, they are prompted the name of the driver and then the name of the city to delete. If all is well, that city is removed from the route of the driver.
+# If the user inputs 5, they are asked for the name of the city they would like to deliver the package to if all is well, a list of all the drivers that deliver to this city is shown.
+
+# Good luck and don’t cry too much 😭 :)
